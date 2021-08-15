4383a169a98972ab216a71ca0d3e94bfa98c6455
Pozdeev <pozdeev.n18@gmail.com>
2021-08-16 00:47:48 +0400

    upload

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+public class JohnMeeting
+{
+    public static string Meeting(string s)
+    {
+        var list = s.ToUpper()
+        .Split(';')
+        .Select(x => x.Split(':'))
+        .OrderBy(x => x[1])
+        .ThenBy(x => x[0])
+        .Select(x => string.Format("({0}, {1})", x[1], x[0]))
+        .ToList();
+        return string.Join("", list);
+    }
+}
